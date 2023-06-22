696dd4d5dbc98adc8f195e40261f86e65b58929f
idim <dim678@ya.ru>
2023-06-22 18:29:58 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ class Student:
         self.age = age
         self.sex = sex
 
+    def __str__(self):
+        return f"Студент: {self.name} --> " \
+               f"возраст: {self.age} лет --> " \
+               f"пол: {self.sex}. "
+
 
 class Student_man(Student):
     def __init__(self, sex, fname, age):
@@ -13,16 +18,14 @@ class Student_man(Student):
 
 with open('final.txt', 'r') as f:
     name = f.readlines()
-    # print(name)
 
 
 def hello(text):
-    print(f'Студент: {student.name}, возраст: {student.age}, пол: {student.sex}')
-    print(f'Студент: {student1.name}, возраст: {student1.age}, пол: {student1.sex}')
+    print(text)
 
 
 if __name__ == '__main__':
     student = Student_man(fname=name[0].rstrip('\n'), age=55, sex='')
     student1 = Student(fname=name[1].rstrip('\n'), age=45, sex='woman')
     hello(student)
-    # hello(student1)
+    hello(student1)
