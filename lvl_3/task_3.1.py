39c66d4204e603c12e2533330d0c6284958784ec
dim447 <dim678@ya.ru>
2023-07-05 11:53:17 +0300

    класс Matrix

--- lvl_3/task_3.1.py
+++ lvl_3/task_3.1.py
@@ -26,3 +26,31 @@
 #   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
 #   - проявите фантазию :)
 
+class Matrix:
+    def __init__(self, rows, cols):
+        self.field = [[0 for _ in range(cols)] for _ in range(rows)]
+        self.rows = rows
+        self.cols = cols
+
+    def get_value(self, row, col):
+        if row in range(self.rows) and col in range(self.cols):
+            return self.field[row][col]
+        return
+
+    def set_value(self, row, col, value):
+        self.field[row][col] = value
+
+    def n_rows(self):
+        return self.rows
+
+    def n_cols(self):
+        return self.cols
+
+
+tab = Matrix(3, 5)
+tab.set_value(1, 2, 3)
+for i in range(tab.n_rows()):
+    for j in range(tab.n_cols()):
+        print(tab.get_value(i, j), end=' ')
+    print()
+print(tab.n_cols())
