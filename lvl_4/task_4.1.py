6875502af381d2a454be96560b23086e6d03f56d
idim <dim678@ya.ru>
2023-07-07 15:21:20 +0300

    Добавил класс Студент, в цикле добавил список студентов в базу.

--- lvl_4/task_4.1.py
+++ lvl_4/task_4.1.py
@@ -23,6 +23,28 @@
 import sqlite3 as sq
 
 
+class Students:
+    def __init__(self, student_id, student_name, school_id):
+        self.student_id = student_id
+        self.student_name = student_name
+        self.school_id = school_id
+
+    def __str__(self):
+        return f"Имя студента: {self.student_name} " \
+               f"ID Студента: {self.student_id} " \
+               f"ID школы: {self.school_id}."
+
+
+# ivan = Students(211, "Костя", 11)
+# print(ivan)
+list_of_students = [
+    [221, 'Sofia', 1],
+    [222, 'Adam', 2],
+    [223, 'Lisa', 3],
+    [224, 'Ignat', 4]
+]
+
+
 def on_startup():
     sql_start()
 
@@ -71,12 +93,12 @@ def sql_delete_command(data):
 if __name__ == '__main__':
     on_startup()
     # sql_add_student((205, "Семен", 5))
-    # sql_add_student((201, "Иван", 1))
-    # sql_add_student((202, "Петр", 2))
-    # sql_add_student((203, "Анастасия", 3))
-    # sql_add_student((204, "Игорь", 4))
     # sql_read_student(204)
     # sql_delete_command("Игорь")
+    # for i in range(len(list_of_students)):
+    #     name_student = Students(list_of_students[i][0], list_of_students[i][1], list_of_students[i][2])
+    #     sql_add_student((name_student.student_id, name_student.student_name, name_student.school_id))
     read_all_students()
+    sql_read_student(222)
 
 ##############################################################
