77fc971d7bf1f607517eab28a0d25d09496f54cd
idim <dim678@ya.ru>
2023-07-21 08:00:36 +0300

    Домашка по sql task_4_1 добавил комментарии к функциям

--- lvl_4/task_4.1.py
+++ lvl_4/task_4.1.py
@@ -22,17 +22,16 @@
 # Название школы:
 import sqlite3 as sq
 
-
-class Students:
-    def __init__(self, student_id, student_name, school_id):
-        self.student_id = student_id
-        self.student_name = student_name
-        self.school_id = school_id
-
-    def __str__(self):
-        return f"Имя студента: {self.student_name} " \
-               f"ID Студента: {self.student_id} " \
-               f"ID школы: {self.school_id}."
+# class Students:
+#     def __init__(self, student_id, student_name, school_id):
+#         self.student_id = student_id
+#         self.student_name = student_name
+#         self.school_id = school_id
+#
+#     def __str__(self):
+#         return f"Имя студента: {self.student_name} " \
+#                f"ID Студента: {self.student_id} " \
+#                f"ID школы: {self.school_id}."
 
 
 # ivan = Students(211, "Костя", 11)
@@ -54,35 +53,31 @@ def sql_start():
     base_connect = sq.connect("/Users/idim/PycharmProjects/project_01/lvl_4/teachers1.db")
     cur = base_connect.cursor()
     if base_connect:
-        pass
-        # print('Data base connected Ok!')
+        # pass
+        print('Data base connected Ok!')
     base_connect.execute(
         'CREATE TABLE IF NOT EXISTS students (student_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, student_name TEXT, school_id INTEGER)')
     base_connect.commit()
-    # base_connect.close()
 
 
-def sql_add_student(data):
+def sql_add_student(data):  # Добавить студенат в базу
     cur.execute('INSERT INTO students (student_id, student_name, school_id) VALUES (?,?,?)', tuple(data))
     base_connect.commit()
-    # base_connect.close()
 
 
-def sql_read_student(number):
+def sql_read_student(number):  # Выввести инфу по студенту по его ID
     ret = cur.execute('SELECT * FROM students where student_id =?', [number]).fetchone()
     print(f'ID Студента: {ret[0]}\nИмя студента: {ret[1]}\nID школы: {ret[2]}')
     base_connect.commit()
-    # base_connect.close()
 
 
-def read_all_students():
+def read_all_students():  # Выввести инфу по всем студентам
     ret = cur.execute('SELECT * FROM students').fetchall()
     for _ in ret:
         print(f'ID Студента: {_[0]}\nИмя студента: {_[1]}\nID школы: {_[2]}\n\n')
-    # base_connect.close()
 
 
-def get_student_by_student(number):
+def get_student_by_student(number):  # Выввести инфу по студенту по его ID JOIN инфу по школе
     ret = cur.execute('SELECT * FROM students '
                       'JOIN School ON School.School_id = students.school_id '
                       'WHERE student_id =?', [number]).fetchone()
@@ -91,20 +86,18 @@ def get_student_by_student(number):
     base_connect.commit()
 
 
-def get_student_by_school(number):
+def get_student_by_school(number):  # Выввести инфу по студенту по ID школы JOIN инфу по школе
     ret = cur.execute('SELECT * FROM students '
                       'JOIN School ON School.School_id = students.school_id '
                       'WHERE School.School_id =?', [number]).fetchall()
-    # print(ret)
     for _ in ret:
         print(f'ID Студента: {_[0]}\nИмя студента: {_[1]}\nID школы: {_[2]}\nНазвание школы: {_[4]}\n')
     base_connect.commit()
 
 
-def sql_delete_student(data):
+def sql_delete_student(data):  # Удалить студента из базы
     cur.execute('DELETE FROM students WHERE student_name == ?', (data,))
     base_connect.commit()
-    # base_connect.close()
 
 
 ##############################################################
